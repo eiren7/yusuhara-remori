b0a8c20b8f577cf87fbf95d8d71fce163d66062c
eiren7 <eiren7@live.com>
2023-10-24 20:58:31 +0900

    deploy

--- src/assets/About.jsx
+++ src/assets/About.jsx
@@ -6,26 +6,32 @@ const About = () => {
       </div>
 
       <div className="about-item-container">
-        <button className="about-item">
-          <img src="https://yusuhararemori.jp/img/top/category01.svg" />
-          <p>ReMORIの取り組み</p>
-        </button>
-        <button className="about-item">
-          <img src="https://yusuhararemori.jp/img/top/category02.svg" />
-          <p>
-            ゆすはら地域おこし
-            <br />
-            協力隊の特徴・制度
-          </p>
-        </button>
-        <button className="about-item">
-          <img src="https://yusuhararemori.jp/img/top/category03.svg" />
-          <p>
-            ゆすはら地域おこし
-            <br />
-            協力隊メンバー紹介
-          </p>
-        </button>
+        <a href="https://yusuhararemori.jp/about.html">
+          <button className="about-item">
+            <img src="https://yusuhararemori.jp/img/top/category01.svg" />
+            <p>ReMORIの取り組み</p>
+          </button>
+        </a>
+        <a href="https://yusuhararemori.jp/training.html">
+          <button className="about-item">
+            <img src="https://yusuhararemori.jp/img/top/category02.svg" />
+            <p>
+              ゆすはら地域おこし
+              <br />
+              協力隊の特徴・制度
+            </p>
+          </button>
+        </a>
+        <a href="https://yusuhararemori.jp/member.html">
+          <button className="about-item">
+            <img src="https://yusuhararemori.jp/img/top/category03.svg" />
+            <p>
+              ゆすはら地域おこし
+              <br />
+              協力隊メンバー紹介
+            </p>
+          </button>
+        </a>
       </div>
     </section>
   )
